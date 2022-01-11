30ec13559a758b3511631730ca6b593b9d6f13a2
Bartosz Buczkowski <bartosz.r.buczkowski@gmail.com>
2022-01-11 23:40:43 +0100

    Add loading spinner

---
+++ src/widgets/punktaWidget/PunktaWidgetList.tsx
@@ -0,0 +1,90 @@
+import { useMemo, useState } from "react";
+import { colors, FormField, formFields } from "./PunktaWidget.data";
+import useCarsForm from "./hooks/useCarsForm";
+import { AutocompleteValues } from "./PunktaWidget.types";
+import { Autocomplete, Box, CircularProgress, TextField } from "@mui/material";
+import useStyles from "./hooks/useStyles";
+
+const orderFields: FormField["id"][] = ["brands", "models", "fuels"];
+const initialFields: Record<FormField["id"], AutocompleteValues | null> = {
+  brands: null,
+  fuels: null,
+  models: null,
+};
+
+function PunktaWidgetList() {
+  const classes = useStyles();
+  const [fieldsValues, setFieldsValues] = useState(initialFields);
+
+  const { brands, fuels, models, loadingFields } = useCarsForm({
+    currentBrand: fieldsValues.brands,
+    currentModel: fieldsValues.models,
+  });
+
+  const options = useMemo(
+    () => ({
+      brands: brands?.map(({ make_code, make_name }) => ({
+        label: make_name,
+        id: make_code,
+      })),
+      models: models?.map(({ model_name }) => ({
+        label: model_name,
+        id: model_name,
+      })),
+      fuels: fuels?.map(({ fuel_code, fuel_name }) => ({
+        label: fuel_name,
+        id: fuel_code,
+      })),
+    }),
+    [brands, fuels, models]
+  );
+
+  const handleFieldsChange = (
+    values: AutocompleteValues | null,
+    id: FormField["id"]
+  ) => {
+    let newFieldsValues = fieldsValues;
+
+    if (!values) {
+      const currentFieldIndex = orderFields.findIndex((field) => id === field);
+      const fieldsToClear = orderFields.slice(currentFieldIndex);
+
+      fieldsToClear.forEach((fieldName) => (newFieldsValues[fieldName] = null));
+    }
+    console.log(newFieldsValues);
+    newFieldsValues[id] = values;
+    setFieldsValues({ ...newFieldsValues });
+  };
+
+  return (
+    <>
+      {formFields.map(({ label, id }) => (
+        <Autocomplete
+          id={id}
+          key={label}
+          disablePortal
+          value={fieldsValues[id]}
+          disabled={!options?.[id]}
+          options={options?.[id] || []}
+          loading={loadingFields[id]}
+          onChange={(_, values) => handleFieldsChange(values, id)}
+          size="small"
+          popupIcon={
+            loadingFields[id] && (
+              <CircularProgress className={classes.spinner} />
+            )
+          }
+          renderInput={(params) => (
+            <TextField
+              {...params}
+              sx={{ background: colors.gray[400] }}
+              label={label}
+            />
+          )}
+        />
+      ))}
+    </>
+  );
+}
+
+export default PunktaWidgetList;
--- src/widgets/punktaWidget/hooks/useStyles.ts
+++ src/widgets/punktaWidget/hooks/useStyles.ts
@@ -3,6 +3,11 @@ import { colors } from "../PunktaWidget.data";
 
 const useStyles = makeStyles(() =>
   createStyles({
+    widgetContainer: {
+      width: 300,
+      height: "fit-content",
+      backgroundColor: "white",
+    },
     submitButton: {
       backgroundColor: colors.yellow[600],
       color: colors.black,
@@ -10,6 +15,16 @@ const useStyles = makeStyles(() =>
       "&:hover": {
         backgroundColor: colors.yellow[700],
       },
+    },
+    listContainer: {
+      display: "flex",
+      flexDirection: "column",
+      gap: "20px",
+      padding: "25px",
+    },
+    buttonContainer: { padding: "25px" },
+    spinner: {
+      padding: "12px",
     },
   })
 );
